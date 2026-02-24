071e1e01466179ecf930d8ac2feaae1c61ecee1a
johanriascos7777 <johanriascos7777@gmail.com>
2026-02-23 22:40:54 -0500

    feat: add TypeORM and PostgreSQL connection setup

--- server/mu-online-api/src/app.module.ts
+++ server/mu-online-api/src/app.module.ts
@@ -1,22 +1,34 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
-import { ItemsModule } from './items/items.module'; 
-import { MapsModule } from './maps/maps.module'; 
+import { ItemsModule } from './items/items.module';
+import { MapsModule } from './maps/maps.module';
 import { CombatModule } from './combat/combat.module';
-
-
-
 
 @Module({
   imports: [
-    CharactersModule,  // como INSTALLED_APPS en Django
-    ItemsModule, // ← agregar
+    // ── TypeORM — conexión a PostgreSQL ──────────────────
+    // synchronize: true → TypeORM crea/actualiza las tablas
+    // automáticamente basándose en las entidades.
+    // ⚠️ Solo para desarrollo — en producción usar migraciones
+    TypeOrmModule.forRoot({
+      type:        'postgres',
+      host:        'localhost',
+      port:        5432,
+      username:    'postgres',
+      password:    '1234567890*',
+      database:    'mu_online',
+      entities:    [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CharactersModule,
+    ItemsModule,
     MapsModule,
     CombatModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers:   [AppService],
 })
 export class AppModule {}
